1f1287b2c17b6711d9f61a5d0d570ffc1598e0df
Petr Joachim <petrjoachim@Petr-MacBook-Air.local>
2021-12-22 23:59:19 +0100

    Add email provider

--- cms/config/env/production/plugins.js
+++ cms/config/env/production/plugins.js
@@ -1,5 +1,4 @@
 module.exports = ({ env }) => ({
-    // ...
     upload: {
       provider: 'cloudinary',
       providerOptions: {
@@ -8,5 +7,18 @@ module.exports = ({ env }) => ({
         api_secret: env('CLOUDINARY_SECRET'),
       },
     },
-    // ...
+    email: {
+      config: {
+        provider: 'mailgun',
+        providerOptions: {
+          apiKey: env('MAILGUN_API_KEY'),
+          domain: env('MAILGUN_DOMAIN'), //Required if you have an account with multiple domains
+          host: env('MAILGUN_HOST', 'api.eu.mailgun.net'), //Optional. If domain region is Europe use 'api.eu.mailgun.net'
+        },
+        settings: {
+          defaultFrom: 'info@lapipi.cz',
+          defaultReplyTo: 'info@lapipi.cz',
+        },
+      },
+    },
   });
