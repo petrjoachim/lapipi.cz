f42330c7f92d26a578b506669c1db3410d7a9401
Petr Joachim <petrjoachim@Petr-MacBook-Air.local>
2021-12-23 08:35:35 +0100

    Fix cloudinary upload

--- cms/config/env/production/plugins.js
+++ cms/config/env/production/plugins.js
@@ -1,12 +1,18 @@
 module.exports = ({ env }) => ({
-    upload: {
+  upload: {
+    config: {
       provider: 'cloudinary',
       providerOptions: {
         cloud_name: env('CLOUDINARY_NAME'),
         api_key: env('CLOUDINARY_KEY'),
         api_secret: env('CLOUDINARY_SECRET'),
       },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
     },
+  },
     email: {
       config: {
         provider: 'sendgrid',
